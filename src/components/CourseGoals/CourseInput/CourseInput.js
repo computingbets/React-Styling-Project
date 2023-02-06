462436c8aa6d8707ce512f2cfcf74eedad5ad197
John Staley <staley77@protonmail.com>
2023-02-06 11:28:49 -0500

    fixing a ui bug w dynamic styling. commenting my understanding of React.js layout.

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -12,15 +12,20 @@ const CourseInput = props => {
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    // onAddGoal passes up to App.js, where data set lives
     props.onAddGoal(enteredValue);
   };
+  // pass 
 
   return (
+
+    // Returns props that fire state funct in first of seq, then state variable to pass data up thru state variable
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
+      {/* imported button component */}
       <Button type="submit">Add Goal</Button>
     </form>
   );
