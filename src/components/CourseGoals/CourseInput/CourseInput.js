783d61fe60ac8e2dcdf28cd78d2d3eaf34620541
John Staley <staley77@protonmail.com>
2023-02-06 12:19:28 -0500

    dynamically style Component to handle errors

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,14 +5,23 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [truValid, setValid] = useState(true);
 
+  // resets ui by setting state to true if there is a value in input
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0 ) {
+      setValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
     // onAddGoal passes up to App.js, where data set lives
+    if (enteredValue.trim().length === 0) {
+      setValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
   // pass 
@@ -22,8 +31,8 @@ const CourseInput = props => {
     // Returns props that fire state funct in first of seq, then state variable to pass data up thru state variable
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: !truValid ? 'red' : 'black' }}>Course Goal</label>
+        <input style={{ borderColor: !truValid ? 'red' : '#ccc', background : !truValid ? 'salmon' : 'transparent' }} type="text" onChange={goalInputChangeHandler} />
       </div>
       {/* imported button component */}
       <Button type="submit">Add Goal</Button>
